e301646a25964ca235bf34bb7cb35feb445d6a5f
Sohail Nasir <jtdinc2000@gmail.com>
2017-10-18 22:49:54 +0100

    Created Start up project with Dependency Injector

---
+++ VNNStart/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using VNNLanguage;
+
+namespace VNNStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = DiContainer.BuildContainer();
+            var dirtyParser = container.Resolve<IParser>();
+            if(dirtyParser.Parse("[Face] says \"hello\""))
+            {
+                Console.WriteLine("Great success");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
