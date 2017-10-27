35f6feb14e2ba78a79604bd6092021b50a91915b
Sohail Nasir <jtdinc2000@gmail.com>
2017-10-28 00:28:56 +0100

    Removed Unnecessry usings

--- VNNStart/Program.cs
+++ VNNStart/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Autofac;
 using VNNLanguage;
 
@@ -14,6 +10,7 @@ namespace VNNStart
         {
             var container = DiContainer.BuildContainer();
             var dirtyParser = container.Resolve<IParser>();
+
             if(dirtyParser.Parse("[Face] says \"hello\""))
             {
                 Console.WriteLine("Great success");
