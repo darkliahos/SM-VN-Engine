027d3a32a7378ab443eaa6b08f7a4639b84f391c
Sohail Nasir <jtdinc2000@gmail.com>
2022-12-09 16:27:38 +0000

    #29-Rewrote start project

---
+++ SMStart/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Autofac;
+using Newtonsoft.Json;
+using SMLanguage;
+using SMLanguage.Models;
+using SMStart;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool debug = args.Any() ? args[0] == "DEBUG" : false;
+
+        var container = DiContainer.BuildContainer();
+        var dirtyParser = container.Resolve<IParser>();
+        var alertHandler = container.Resolve<IAlertHandler>();
+        try
+        {
+            var metadata = GetMetadataInfo(debug);
+            GameState.Instance.SetupGameState(metadata, debug);
+
+            using Stage stage = new(800, 600, metadata.Title, dirtyParser);
+            stage.Run();
+
+
+        }
+        catch (Exception error)
+        {
+            alertHandler.ShowError(error);
+        }
+        Console.ReadLine();
+    }
+
+    private static Metadata GetMetadataInfo(bool debug)
+    {
+        var path = $"{Directory.GetCurrentDirectory()}\\Metadata.json";
+
+        if (!File.Exists(path))
+        {
+            string errorMessage = debug ? "Please use the metadata geneator to generate your file" : "Error finding Metadata.json, please contact your vendor";
+            throw new FileNotFoundException(errorMessage);
+        }
+
+        //TODO: Need to do some validation on version hashing but that will be later
+
+        return JsonConvert.DeserializeObject<Metadata>(File.ReadAllText(path));
+    }
+}
