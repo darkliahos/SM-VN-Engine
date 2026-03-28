59a64a26c5f4464630c4e16f3de14137dfb61543
darkliahos <jtdinc2000@gmail.com>
2026-03-28 17:15:03 +0000

    Removed deprecated Utils

--- SM-ElectronContainer/src/services/StringUtils.ts
+++ SM-ElectronContainer/src/services/StringUtils.ts
@@ -1,13 +1,3 @@
 export function containsInsensitive(input: string, value: string): boolean {
   return input.toLowerCase().includes(value.toLowerCase());
 }
-
-export function findAndGetKeywords(input: string, keywords: string[]): string {
-  const pattern = new RegExp(`\\b(${keywords.join('|')})\\b`, 'i');
-  const match = input.match(pattern);
-  return match ? match[0] : '';
-}
-
-export function toEnum<T>(value: string): T {
-  return value as unknown as T;
-}
--- SM-ElectronContainer/src/services/index.ts
+++ SM-ElectronContainer/src/services/index.ts
@@ -3,4 +3,4 @@ export { ConsoleAlertHandler, IAlertHandler } from './AlertHandler';
 export { StateManager, IStateManager } from './StateManager';
 export { DirtyParser, IParser } from './Parser';
 export { RegexConstants } from './RegexConstants';
-export { containsInsensitive, findAndGetKeywords, toEnum } from './StringUtils';
+export { containsInsensitive} from './StringUtils';
